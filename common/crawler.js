5eafb4d1144573fbefafaca3e3748ef7bc733fbf
xinghaiyang <xinghaiyang@daojia-inc.com>
2019-08-15 23:25:53 +0800

    feat 字符集

--- common/crawler.js
+++ common/crawler.js
@@ -1,5 +1,6 @@
 // 爬虫
 const superagent = require('superagent')
+require('superagent-charset')(superagent)
 const cheerio = require('cheerio')
 const log = require('./log')
 const email  = require('./email')
@@ -8,6 +9,7 @@ const { userAgents } = require('../utils/const')
 const { rnd } = require('../utils/tools')
 const debug = require('debug')
 const logCrawler = debug('crawler')
+
 
 const {
   findChannelAll,
@@ -37,6 +39,42 @@ function renderUrl(listUrlRule, info) {
   return res
 }
 
+// 请求
+function fetchPage(info) {
+  return new Promise(function (resolve, reject) {
+    const { 
+      domain,
+      hotUrl,
+      cookie,
+      useUserAgent,
+      charset
+    } = info
+    let request = superagent.get(domain + hotUrl)
+
+    // 设置 cookie
+    if (cookie !== '') {
+      request = request.set('cookie', cookie)
+    }
+
+    // 设置 userAgent
+    if (useUserAgent !== 0) {
+      request = request.set('User-Agent', userAgents[rnd(0, userAgents.length - 1)])
+    }
+
+    // 设置 字符集
+    if (charset !== '') {
+      request = request.charset(charset).buffer(true)
+    } 
+
+    request.end((err, res) => {
+      if (err) {
+        return reject(err)
+      }
+      resolve(res)
+    })
+  })
+}
+
 // 抓取单个页面中的列表
 async function fetchHotList(info) {
   let result
@@ -44,48 +82,18 @@ async function fetchHotList(info) {
     id,
     name,
     domain,
-    hotUrl,
-    cookie,
-    useUserAgent,
     listSpecialMethod,
     listDom,
     listTitleDom,
     listUrlDom,
     listUrlRule
   } = info
-  let userAgent = userAgents[rnd(0, userAgents.length - 1)]
   let titleDomHasChildren = listTitleDom !== ''
   let urlDomHasChildren = listUrlDom !== ''
-  
-  // logCrawler(useUserAgent)
 
   try {
-    let res 
-    if (cookie === ''){
-      // 不携带 cookie
-      if (useUserAgent === 0) {
-        res = await superagent
-          .get(domain + hotUrl)
-      } else {
-        res = await superagent
-        .get(domain + hotUrl)
-        .set('User-Agent', userAgent)
-      }
-    } else {
-      // 携带 cookie
-      if (useUserAgent === 0) {
-        res = await superagent
-          .get(domain + hotUrl)
-          .set('cookie', cookie)
-      } else {
-        res = await superagent
-          .get(domain + hotUrl)
-          .set('cookie', cookie)
-          .set('User-Agent', userAgent)
-      }
-    }
-
-    let $ = cheerio.load(res.text)
+    let resHtml = await fetchPage(info) 
+    let $ = cheerio.load(resHtml.text)
     let originList = []
 
     if (listSpecialMethod !== '') {
@@ -96,7 +104,7 @@ async function fetchHotList(info) {
       $(listDom).each((index, el) => {
         let titleDom = titleDomHasChildren ? $(el).find(listTitleDom) : $(el) 
         let urlDom = urlDomHasChildren ? $(el).find(listUrlDom) : $(el) 
-  
+
         originList.push({
           title: titleDom.text().replace(/\s/g, ''),
           domOriginUrl: urlDom.attr('href')
@@ -120,7 +128,6 @@ async function fetchHotList(info) {
     })
 
     // return logCrawler(list)
-
     result = await insertList(list)
   } catch (err) {
     log.logToFile('crawler.log', `${name}|${(new Date()).toLocaleString()}|${err.message}`)
