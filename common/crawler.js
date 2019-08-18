4f98c6c1768f4f9a446b544c77ef2a22bb9e6068
xinghaiyang <xinghaiyang@daojia-inc.com>
2019-08-18 19:01:43 +0800

    feat 调整spa抓取

--- common/crawler.js
+++ common/crawler.js
@@ -22,8 +22,20 @@ const {
   deleteListByChannelId
 }  = require('../dao/list')
 
-let str = '{domain}{listUrlDom}'
-str.match(/\{.*\}/g)
+// 统一抓取错误处理
+function dealFetchError(info, err) {
+  const { 
+    name
+  } = info
+  log.logToFile('crawler.log', `${name}|${(new Date()).toLocaleString()}|${err.message}`)
+  // 正式环境发邮件
+  if (env !== 'local') {
+    email.send({
+      subject: '爬虫抓取失败',
+      html: `${name}|${(new Date()).toLocaleString()}|${err.message}`
+    })
+  }
+}
 
 // 根据 listUrlRule 生成 url
 function renderUrl(listUrlRule, info) {
@@ -40,8 +52,85 @@ function renderUrl(listUrlRule, info) {
   return res
 }
 
-// 普通页面抓取
-function fetchPage(info) {
+// 获取数组并插入
+async function htmlToList (info, resHtml) {
+  const { 
+    id,
+    domain,
+    listSpecialMethod,
+    listDom,
+    listTitleDom,
+    listUrlDom,
+    listUrlRule
+  } = info
+  let result
+  let titleDomHasChildren = listTitleDom !== ''
+  let urlDomHasChildren = listUrlDom !== ''
+
+  let $ = cheerio.load(resHtml)
+  let originList = []
+
+  if (listSpecialMethod !== '') {
+    // 按特殊方法获取列表
+    originList = specialChannelMethod[listSpecialMethod]($, info)
+  } else {
+    // 按常规
+    $(listDom).each((index, el) => {
+      let titleDom = titleDomHasChildren ? $(el).find(listTitleDom) : $(el) 
+      let urlDom = urlDomHasChildren ? $(el).find(listUrlDom) : $(el) 
+
+      originList.push({
+        title: titleDom.text().replace(/\s/g, ''),
+        domOriginUrl: urlDom.attr('href')
+      })
+    })
+  }
+
+  if (originList.length === 0) {
+    throw new Error('未抓取数据！')
+  }
+  let list = originList.map(({ title, domOriginUrl }, index) => {
+    return {
+      channelId: id,
+      sort: index,
+      title,
+      url: renderUrl(listUrlRule, {
+        domain,
+        listUrlDom: domOriginUrl
+      })
+    }
+  })
+
+  // return logCrawler(list)
+  result = await insertList(list)
+  return (Object.assign([], result)).length > 0 
+}
+
+// 抓取单个 SPA 页面
+async function fetchSpaPage(info, browser) {
+  const { 
+    domain,
+    hotUrl,
+    listTitleDom
+  } = info
+  let resIsTrue = false
+  try {
+    // 获取内容
+    const page = await browser.newPage()    // 创建一个 Page 实例
+    await page.goto(domain + hotUrl)   // 进入网址
+    await page.waitFor(listTitleDom)    // 等待目标元素出现，用于客户端渲染页面
+    let resHtml = await page.content()
+
+    // 处理数据并入库
+    resIsTrue = await htmlToList(info, resHtml)
+  } catch (err) {
+    dealFetchError(info, err)
+  }
+  return resIsTrue
+}
+
+// 获取单个普通页面内容
+function fetchCommonePageContent(info) {
   return new Promise(function (resolve, reject) {
     const { 
       domain,
@@ -76,99 +165,54 @@ function fetchPage(info) {
   })
 }
 
-// SPA 页面抓取
-async function fetchSpa(info) {
-  const { 
-    domain,
-    hotUrl,
-    listTitleDom
-  } = info
-  const browser = await puppeteer.launch({
-    args: ['--no-sandbox'],
-  });
-  const page = await browser.newPage()
-  await page.goto(domain + hotUrl)
-  await page.waitFor(listTitleDom)
-
-  let html = await page.content()
-  await browser.close()
-  return html
-}
-
-// 抓取单个页面中的列表
-async function fetchHotList(info) {
-  let result
-  const { 
-    id,
-    name,
-    domain,
-    isSpa,
-    listSpecialMethod,
-    listDom,
-    listTitleDom,
-    listUrlDom,
-    listUrlRule
-  } = info
-  let titleDomHasChildren = listTitleDom !== ''
-  let urlDomHasChildren = listUrlDom !== ''
-
+// 抓取单个普通页面
+async function fetchCommonPage(info) {
+  let resIsTrue = false
+  // logCrawler('fetchCommonPage', info)
   try {
-    let resHtml
-    if (isSpa === 0) {
-      // 不是单页
-      resHtml = await fetchPage(info) 
-    } else {
-      // 单页
-      resHtml = await fetchSpa(info) 
-    }
-    let $ = cheerio.load(resHtml)
-    let originList = []
+    
+    // 获取内容
+    let resHtml = await fetchCommonePageContent(info) 
 
-    if (listSpecialMethod !== '') {
-      // 按特殊方法获取列表
-      originList = specialChannelMethod[listSpecialMethod]($, info)
-    } else {
-      // 按常规
-      $(listDom).each((index, el) => {
-        let titleDom = titleDomHasChildren ? $(el).find(listTitleDom) : $(el) 
-        let urlDom = urlDomHasChildren ? $(el).find(listUrlDom) : $(el) 
-
-        originList.push({
-          title: titleDom.text().replace(/\s/g, ''),
-          domOriginUrl: urlDom.attr('href')
-        })
-      })
-    }
-
-    if (originList.length === 0) {
-      throw new Error('未抓取数据！')
-    }
-    let list = originList.map(({ title, domOriginUrl }, index) => {
-      return {
-        channelId: id,
-        sort: index,
-        title,
-        url: renderUrl(listUrlRule, {
-          domain,
-          listUrlDom: domOriginUrl
-        })
-      }
-    })
-
-    // return logCrawler(list)
-    result = await insertList(list)
+    // 处理数据并入库
+    resIsTrue = await htmlToList(info, resHtml)
   } catch (err) {
-    log.logToFile('crawler.log', `${name}|${(new Date()).toLocaleString()}|${err.message}`)
-    // 正式环境发邮件
-    if (env !== 'local') {
-      email.send({
-        subject: '爬虫抓取失败',
-        html: `${name}|${(new Date()).toLocaleString()}|${err.message}`
-      })
-    }
+    dealFetchError(info, err)
   }
   // logCrawler('res', result)
-  return (Object.assign([], result)).length > 0
+  return resIsTrue
+}
+
+// Promise.all 处理动态数组
+async function arrPromise(arr = [], fn, ...rest) {
+  // logCrawler('fn', rest)
+  let arrPromise = arr.map(async el => {
+    let isTrue = await fn(el, ...rest)
+    return isTrue
+  })
+  let arrIsTrue = await Promise.all(arrPromise)
+  return arrIsTrue.every(el => el)
+}
+
+// 处理渠道数组并抓取
+async function dealAllChannel(arrChannel = []) {
+  // 遍历抓取普通页面插入列表
+  let arrCommonChannel = arrChannel.filter(el => el.isSpa === 0)
+  let commonRes = await arrPromise(arrCommonChannel, fetchCommonPage)
+
+  // 抓取所有 spa 页面
+  let arrSpaChannel = arrChannel.filter(el => el.isSpa === 1)
+  let spaRes = true
+  if (arrSpaChannel.length > 0) {
+    const browser = await puppeteer.launch({
+      args: ['--no-sandbox']
+    })
+    spaRes = await arrPromise(arrSpaChannel, fetchSpaPage, browser)
+    await browser.close()
+  }
+
+  // logCrawler('dealAllChannel', commonRes)
+  return commonRes && spaRes
 }
 
 module.exports = {
@@ -179,16 +223,9 @@ module.exports = {
 
     // 删除所有列表
     await deleteListAll()
-    
-    // 遍历抓取插入列表
-    let arrIsOkPromise = list.map(async el => {
-      let isOk = await fetchHotList(el)
-      return isOk
-    })
-    let arrIsOk = await Promise.all(arrIsOkPromise)
-
-    // logCrawler('arrIsOk', arrIsOk)
-    return arrIsOk.every(el => el)
+   
+    // logCrawler('list', list)
+    return dealAllChannel(list)
   },
   async fetchSingleData(channelId) {
     // 获取单个渠道详情
@@ -202,6 +239,6 @@ module.exports = {
     await deleteListByChannelId(channelId)
     
     // 抓取插入新列表
-    return fetchHotList(detail)
+    return dealAllChannel([detail])
   }
 }
