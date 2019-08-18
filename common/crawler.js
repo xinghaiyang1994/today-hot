90a96d8f262139f286ac8a1768e654fe6a7dcddb
xinghaiyang <xinghaiyang@daojia-inc.com>
2019-08-19 00:32:05 +0800

    test

--- common/crawler.js
+++ common/crawler.js
@@ -113,17 +113,19 @@ async function fetchSpaPage(info, browser) {
   const { 
     domain,
     hotUrl,
-    listTitleDom
+    listDom
   } = info
   let resIsTrue = false
+  let wrapDom = listDom.split(/\s/)[0]
+  console.log(wrapDom)
   try {
     // 获取内容
     const page = await browser.newPage()    // 创建一个 Page 实例
     await page.goto(domain + hotUrl)   // 进入网址
-    await page.waitFor(listTitleDom)    // 等待目标元素出现，用于客户端渲染页面
-    let resHtml = await page.evaluate(() => {
-      return document.querySelector(listTitleDom.split(/\s/)[0]).innerHTML
-    })
+    await page.waitFor(listDom)    // 等待目标元素出现，用于客户端渲染页面
+    let resHtml = await page.evaluate(dom => {
+      return document.querySelector(dom).innerHTML
+    }, wrapDom)
 
     // 处理数据并入库
     resIsTrue = await htmlToList(info, resHtml)
