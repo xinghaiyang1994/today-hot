af336ff4517bbafc1890a7097a5e8337d0612232
xinghaiyang <xinghaiyang@daojia-inc.com>
2019-08-21 12:50:08 +0800

    modify

--- common/time.js
+++ common/time.js
@@ -5,7 +5,8 @@ const crawler = require('./crawler')
 // 每个小时抓取更新所有列表
 schedule.scheduleJob('0 0 * * * *', async () => {
   const { isTrue, startTime } = await crawler.fetchAllData()
-  let log = '每小时抓取：' + isTrue ? `成功，耗时 ${Date.now() - startTime} ms` : '失败！'
+  let log = isTrue ? `成功，耗时 ${Date.now() - startTime} ms` : '失败！'
+  log =  '每小时抓取：' + log
   console.log(log)
 })
 
