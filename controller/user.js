4018079ccb547d89ccf01ead19fa7e56084e6e5a
xinghaiyang <xinghaiyang@daojia-inc.com>
2019-11-13 00:49:19 +0800

    daily 用户信息及退出

--- controller/user.js
+++ controller/user.js
@@ -1,9 +1,16 @@
 const { dealBody } = require('../utils/tools')
+const { checkAdmin } = require('../middlewares/check')
 
 module.exports = {
   // 登录用户信息
   async getInfo(ctx) {
     console.log(ctx.session)
+    // 检查是否是管理员
+    const { status, data } = checkAdmin(ctx)
+    if (!status) {
+      return ctx.body = data
+    }
+
     const { id, name } = ctx.session.user
 
     return ctx.body = dealBody({
--- middlewares/check.js
+++ middlewares/check.js
@@ -1,7 +1,46 @@
+const { dealBody } = require('../utils/tools')
 module.exports = {
-    checkLogin: ctx => {
-        if (!ctx.session || !ctx.session.name) {
-            throw new Error('用户未登录，请登录后再试!')
-        }
+  // 检查是否登录
+  checkLogin(ctx) {
+    if (!ctx.session.user) {
+      return {
+        status: false,
+        data: dealBody({
+          code: -2,
+          message: '用户未登录，请登录后再试！'
+        })
+      }
     }
+
+    return {
+      status: true,
+      data: ''
+    }
+  },
+  // 检查是否是管理员(包含检查登录)
+  checkAdmin(ctx) {
+    if (!ctx.session.user) {
+      return {
+        status: false,
+        data: dealBody({
+          code: -2,
+          message: '用户未登录，请登录后再试！'
+        })
+      }
+    }
+    if (ctx.session.user.isAdmin !== 1) {
+      return {
+        status: false,
+        data: dealBody({
+          code: -3,
+          message: '暂无权限！'
+        })
+      }
+    }
+
+    return {
+      status: true,
+      data: ''
+    }
+  },
 }
